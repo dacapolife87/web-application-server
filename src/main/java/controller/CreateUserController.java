57896741de556b84c6d1c760dddd52ab21ebf378
hJJANG <dacapolife87@gmail.com>
2020-09-28 07:43:05 +0900

    feat : Controller 기능 구현체 추가

---
+++ src/main/java/controller/CreateUserController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import http.HttpRequest;
+import http.HttpResponse;
+
+public class CreateUserController implements Controller {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {
+
+    }
+}
---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import http.HttpRequest;
+import http.HttpResponse;
+
+public class LoginController implements Controller {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {
+
+    }
+}
